af3e735aad8d67ed84ed30104c3b0e0138e2395b
Pawel Cebula <ptcebula@gmail.com>
2021-08-26 17:05:01 +0200

    style: iAdd a wrapper for header footer and each template

--- src/filters/dateFilter.js
+++ src/filters/dateFilter.js
@@ -1,7 +1,4 @@
 module.exports = function(date) {
   const addZero = i => String(i).length === 1 ? `0${String(i)}`: String(i);
-  const month = addZero(date.getMonth() + 1);
-  const day = addZero(date.getDate());
-  console.log(month, day);
   return `${date.getFullYear()}-${addZero(date.getMonth() + 1)}-${addZero(date.getDate())}`;
 }
