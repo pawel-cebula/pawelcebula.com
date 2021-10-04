a6185e45aac9d83a46b12edfe95c55a51713dc1e
Pawel Cebula <hi@pawelcebula.com>
2021-10-04 12:59:02 +0200

    feat: Implement SEO and social meta tags

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,24 @@
 const dateFilter = require('./src/filters/dateFilter.js');
 const readingTime = require('eleventy-plugin-reading-time');
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const pluginSEO = require('eleventy-plugin-seo');
 
 module.exports = (config) => {
   config.setBrowserSyncConfig({ ghostMode: false });
   config.addPlugin(readingTime);
   config.addPlugin(syntaxHighlight, {
     templateFormats: ['md'],
+  });
+  config.addPlugin(pluginSEO, {
+    title: 'Pawel Cebula',
+    description: 'Web developer and startup founder based in Berlin',
+    url: 'https://pawelcebula.com',
+    author: 'Pawel Cebula',
+    twitter: 'pawelceb',
+    image: '/assets/personalwebsite.png',
+    options: {
+      titleDivider: '|',
+    },
   });
   config.addFilter('dateFilter', dateFilter);
   config.addPassthroughCopy({ public: './' });
