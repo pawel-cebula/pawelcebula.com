20b728f2ac61b637a289b03ebd6d80acc60dc753
pawelcebula <hi@pawelcebula.com>
2023-08-30 13:15:03 +0200

    update about and projects

--- src/_data/bio.js
+++ src/_data/bio.js
@@ -1,6 +1,6 @@
 module.exports = [
-  'I started learning to code in 2019 out of curiosity, wanting to have a deeper understanding of how our tech was built.',
-  'Very quickly, curiosity turned into passion and after a few months of learning and building, I knew that I want to become a full-time software developer.',
-  "After starting my learning journey with Python and Django, I have been focusing on web development with HTML, CSS, JavaScript, TypeScript, Node.js, Express, React, Next.js, PostgreSQL and GraphQL. Along with my primary stack, I'm always learning about new technologies and have recently been exploring Prisma, ElasticSearch and Tailwind CSS.",
-  'I love solving problems and as a developer I can do that in a more scalable way, whether working with others or on personal projects.',
+  "I started learning to code in 2019 out of curiosity, wanting to have a deeper understanding of how our tech was built.",
+  "Very quickly, curiosity turned into passion and after a few months of learning and building, I knew that I want to become a full-time software engineer.",
+  "After starting my learning journey with Python and Django, I have been focusing on web development with HTML, CSS, JavaScript, TypeScript, Node.js, Express, React, Next.js, PostgreSQL and GraphQL. Along with my primary stack, I'm always learning about new technologies and have recently been exploring ML & AI.",
+  "I love solving problems and as an engineer I can do that in a more scalable way, whether working with others or on personal projects.",
 ];
